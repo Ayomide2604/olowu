a22211516ad15d6b9b23b5a3fdce95c0750d7163
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2026-08-03 07:58:25 -0600

    removed unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import Header from "@/components/layout/Header";
-import BottomNav from "@/components/layout/BottomNav";
-
 import { AuthProvider } from "@/context/AuthProvider";
 
 export const metadata: Metadata = {
